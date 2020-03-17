35e6e3de4ede36323621671c8fa6c425330ca826
shensong <shensong@lixiang.com>
2020-03-17 18:34:54 +0800

    update with structure-tree
    Signed-off-by: shensong <shensong@lixiang.com>

--- other/example.py
+++ other/example.py
@@ -1,81 +1,120 @@
-import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon, QBrush, QColor
-from PyQt5.QtCore import Qt
+# import sys
+# from PyQt5.QtWidgets import *
+# from PyQt5.QtGui import QIcon, QBrush, QColor
+# from PyQt5.QtCore import Qt
+#
+#
+# class TreeWidgetDemo(QMainWindow):
+#     def __init__(self, parent=None):
+#         super(TreeWidgetDemo, self).__init__(parent)
+#         self.setWindowTitle('TreeWidget 例子')
+#
+#         self.tree=QTreeWidget()
+#         tree_qss = 'QTreeWidget::branch:closed:has-children{image:url(:root_close.png);} \
+#                     QTreeWidget::branch:open:has-children{image:url(:root_open.png);}'
+#
+#         self.tree.setStyleSheet(tree_qss)
+#         #设置列数
+#         self.tree.setColumnCount(2)
+#         #设置树形控件头部的标题
+#         self.tree.setHeaderLabels(['Key','Value'])
+#
+#         #设置根节点
+#         root=QTreeWidgetItem(self.tree)
+#         root.setText(0,'Root')
+#         root.setIcon(0,QIcon('./images/root.png'))
+#
+#         # todo 优化2 设置根节点的背景颜色
+#         brush_red=QBrush(Qt.red)
+#         root.setBackground(0,brush_red)
+#         brush_blue=QBrush(Qt.blue)
+#         root.setBackground(1,brush_blue)
+#
+#         #设置树形控件的列的宽度
+#         self.tree.setColumnWidth(0,150)
+#
+#         #设置子节点1
+#         child1=QTreeWidgetItem()
+#         child1.setText(0,'child1')
+#         child1.setText(1,'ios')
+#         child1.setIcon(0,QIcon('./images/IOS.png'))
+#
+#         #todo 优化1 设置节点的状态
+#         child1.setCheckState(0,Qt.Checked)
+#
+#         root.addChild(child1)
+#
+#
+#         #设置子节点2
+#         child2=QTreeWidgetItem(root)
+#         child2.setText(0,'child2')
+#         child2.setText(1,'')
+#         child2.setIcon(0,QIcon('./images/android.png'))
+#
+#         #设置子节点3
+#         child3=QTreeWidgetItem(child2)
+#         child3.setText(0,'child3')
+#         child3.setText(1,'android')
+#         child3.setIcon(0,QIcon('./images/music.png'))
+#
+#
+#
+#         #加载根节点的所有属性与子控件
+#         self.tree.addTopLevelItem(root)
+#
+#         #TODO 优化3 给节点添加响应事件
+#         self.tree.clicked.connect(self.onClicked)
+#
+#
+#         #节点全部展开
+#         self.tree.expandAll()
+#         self.setCentralWidget(self.tree)
+#
+#     def onClicked(self,qmodeLindex):
+#         item=self.tree.currentItem()
+#         print('Key=%s,value=%s'%(item.text(0),item.text(1)))
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     tree = TreeWidgetDemo()
+#     tree.show()
+#     sys.exit(app.exec_())
 
 
-class TreeWidgetDemo(QMainWindow):
-    def __init__(self, parent=None):
-        super(TreeWidgetDemo, self).__init__(parent)
-        self.setWindowTitle('TreeWidget 例子')
-
-        self.tree=QTreeWidget()
-        tree_qss = 'QTreeWidget::branch:closed:has-children{image:url(:root_close.png);} \
-                    QTreeWidget::branch:open:has-children{image:url(:root_open.png);}'
-
-        self.tree.setStyleSheet(tree_qss)
-        #设置列数
-        self.tree.setColumnCount(2)
-        #设置树形控件头部的标题
-        self.tree.setHeaderLabels(['Key','Value'])
-
-        #设置根节点
-        root=QTreeWidgetItem(self.tree)
-        root.setText(0,'Root')
-        root.setIcon(0,QIcon('./images/root.png'))
-
-        # todo 优化2 设置根节点的背景颜色
-        brush_red=QBrush(Qt.red)
-        root.setBackground(0,brush_red)
-        brush_blue=QBrush(Qt.blue)
-        root.setBackground(1,brush_blue)
-
-        #设置树形控件的列的宽度
-        self.tree.setColumnWidth(0,150)
-
-        #设置子节点1
-        child1=QTreeWidgetItem()
-        child1.setText(0,'child1')
-        child1.setText(1,'ios')
-        child1.setIcon(0,QIcon('./images/IOS.png'))
-
-        #todo 优化1 设置节点的状态
-        child1.setCheckState(0,Qt.Checked)
-
-        root.addChild(child1)
+# -*- coding: UTF-8 -*-
+# 从文件中读取数据
+import xml.etree.ElementTree as ET
 
 
-        #设置子节点2
-        child2=QTreeWidgetItem(root)
-        child2.setText(0,'child2')
-        child2.setText(1,'')
-        child2.setIcon(0,QIcon('./images/android.png'))
-
-        #设置子节点3
-        child3=QTreeWidgetItem(child2)
-        child3.setText(0,'child3')
-        child3.setText(1,'android')
-        child3.setIcon(0,QIcon('./images/music.png'))
-
-
-
-        #加载根节点的所有属性与子控件
-        self.tree.addTopLevelItem(root)
-
-        #TODO 优化3 给节点添加响应事件
-        self.tree.clicked.connect(self.onClicked)
+# 遍历所有的节点
+def walkData(root_node, level, result_list):
+    temp_list = [level, root_node.tag, root_node.attrib]
+    result_list.append(temp_list)
+    # 遍历每个子节点
+    children_node = root_node.getchildren()
+    if len(children_node) == 0:
+        return
+    for child in children_node:
+        walkData(child, level + 1, result_list)
+    return
 
 
-        #节点全部展开
-        self.tree.expandAll()
-        self.setCentralWidget(self.tree)
+def getXmlData(file_name):
+    level = 0  # 节点的深度(层)从0开始
+    result_list = []
+    root = ET.parse(file_name).getroot()
+    walkData(root, level, result_list)
+    return result_list
 
-    def onClicked(self,qmodeLindex):
-        item=self.tree.currentItem()
-        print('Key=%s,value=%s'%(item.text(0),item.text(1)))
+
+# def update_(result_list):
+#     for result in result_list:
+#         if result[0] == 1:
+
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    tree = TreeWidgetDemo()
-    tree.show()
-    sys.exit(app.exec_())
+    file_name = 'D:/Code/editor/xml/test.xml'
+    R = getXmlData(file_name)
+    for x in R:
+        print(x)
+    pass
--- ui_class/TreeStructure.py
+++ ui_class/TreeStructure.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from collections import OrderedDict
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -12,6 +13,10 @@ class TreeStructure(QWidget):
         super(TreeStructure, self).__init__(parent)
         # 传入的文件
         self.xml_file = xml_file
+        # 保存上一次的树数据
+        self.old_tree_data = []
+        # 子节点
+        self.child_list = []
         # 设置背景色
         self.setStyleSheet('background-color: #F0F0F0; font-family:Arial;')
         # 控件title
@@ -44,61 +49,122 @@ class TreeStructure(QWidget):
 
         self.setLayout(self.general_layout)
 
-    # 更新树形结构视图
-    def update_structure(self, xml_file):
-        self.xml_file = xml_file
-        # 重命名根节点名字(当前文件名)
-        root_name = 'None' if self.xml_file == 'None' else os.path.split(self.xml_file)[1]
-        self.root.setText(0, root_name)
+    # 添加节点
+    def add_node(self, parent=None, node_name=None):
+        child = QTreeWidgetItem(parent)
+        child.setText(0, node_name)
+        child.setIcon(0, QIcon(Icon.xml_tag))
+        return child
+
+    # 获取节点name
+    def get_node_name(self, tag, attrib):
+        text = tag
+        # 如果存在属性
+        if len(attrib):
+            for key in attrib.keys():
+                if key == 'id':
+                    text = attrib[key] + ':' + text
+                elif key == 'name':
+                    if 'id' in attrib.keys():
+                        text = text + ' name:' + attrib[key]
+                    else:
+                        text = attrib[key] + ':' + text
+                else:
+                    text = text + ' ' + key + ':' + attrib[key]
+        return text
+
+    # 节点遍历
+    def node_traverse(self, root_node, level, result_list):
+        temp_list = [level, root_node.tag, root_node.attrib]
+        result_list.append(temp_list)
+        # 遍历每个子节点
+        children_node = root_node.getchildren()
+        if len(children_node) == 0:
+            return
+        for child in children_node:
+            self.node_traverse(child, level + 1, result_list)
+        return
+
+    # 获取树数据
+    def get_tree_data(self, xml_file):
+        # 保存树数据
+        result_list = []
         try:
             # 根据xml文本获取子节点
-            tree = ET.ElementTree(file=self.xml_file)
-            self.tree.clear()
-            # 设置根节点
-            self.root = QTreeWidgetItem(self.tree)
-            self.root.setText(0, root_name)
-            self.tree.addTopLevelItem(self.root)
-            # 添加xml文件的根节点
+            tree = ET.ElementTree(file=xml_file)
             xml_root = tree.getroot()
-            # 添加xml根标签(文件名节点之下)
-            parent = self.add_item(self.root, xml_root)
-            # 通过遍历添加子节点
-            for child_of_1 in xml_root:
-                child_of_1_as_root = self.add_item(parent, child_of_1)
-                for child_of_2 in child_of_1:
-                    child_of_2_as_root = self.add_item(child_of_1_as_root, child_of_2)
-                    for child_of_3 in child_of_2:
-                        child_of_3_as_root = self.add_item(child_of_2_as_root, child_of_3)
-                        for child_of_4 in child_of_3:
-                            child_of_4_as_root = self.add_item(child_of_3_as_root, child_of_4)
-                            for child_of_5 in child_of_4:
-                                child_of_5_as_root = self.add_item(child_of_4_as_root, child_of_5)
-                                for child_of_6 in child_of_5:
-                                    self.add_item(child_of_5_as_root, child_of_6)
-            self.tree.expandAll()
+            level = 0  # 节点的深度从0开始(0是case标签)
+            self.node_traverse(xml_root, level, result_list)
         except ET.ParseError:
             pass
         except FileNotFoundError:
             pass
+        finally:
+            return result_list
 
-    # 添加节点
-    def add_item(self, parent, info):
-        tag = info.tag
-        info_dict = info.attrib
-        text = tag
-        # 如果存在属性
-        if len(info_dict):
-            for key in info_dict.keys():
-                if key == 'id':
-                    text = info_dict[key] + ':' + text
-                elif key == 'name':
-                    if 'id' in info_dict.keys():
-                        text = text + ' name:' + info_dict[key]
-                    else:
-                        text = info_dict[key] + ':' + text
-                else:
-                    text = text + ' ' + key + ':' + info_dict[key]
-        child = QTreeWidgetItem(parent)
-        child.setText(0, text)
-        child.setIcon(0, QIcon(Icon.xml_tag))
-        return child
+    # 新建树结构
+    def new_structure(self, tree_data):
+        for note_data in tree_data:
+            level, tag, attrib = note_data
+            node_name = self.get_node_name(tag, attrib)
+            if level == 0:
+                xml_root = self.add_node(self.root, node_name)
+                self.child_list.append(xml_root)
+            elif level == 1:
+                first_node = self.add_node(xml_root, node_name)
+                self.child_list.append(first_node)
+            elif level == 2:
+                second_node = self.add_node(first_node, node_name)
+                self.child_list.append(second_node)
+            elif level == 3:
+                third_node = self.add_node(second_node, node_name)
+                self.child_list.append(third_node)
+            elif level == 4:
+                fourth_node = self.add_node(third_node, node_name)
+                self.child_list.append(fourth_node)
+            elif level == 5:
+                fifth_node = self.add_node(fourth_node, node_name)
+                self.child_list.append(fifth_node)
+            elif level == 6:
+                sixth_node = self.add_node(fifth_node, node_name)
+                self.child_list.append(sixth_node)
+
+    # 动态更新结构图
+    def dynamic_update_structure(self, current_tree_data):
+        old_tree_data_length = len(self.old_tree_data)
+        current_tree_data_length = len(current_tree_data)
+        # 增加标签
+        if current_tree_data_length > old_tree_data_length:
+            for i in range(current_tree_data_length):
+                if current_tree_data[i] not in self.old_tree_data:
+                    level, tag, attrib = current_tree_data[i]
+                    node_name = self.get_node_name(tag, attrib)
+                    child = self.add_node(node_name=node_name)
+                    if level == 1:
+                        previous_level = current_tree_data[i-1][0]
+                        if previous_level == 1:
+                            index = self.root.child(0).indexOfChild(self.child_list[i-1])
+                        self.root.child(0).insertChild(index+1, child)
+                        self.child_list.insert(i, child)
+        # 删除标签
+        elif current_tree_data_length < old_tree_data_length:
+            pass
+        # 更改标签
+        elif current_tree_data_length == old_tree_data_length:
+            pass
+
+    # 更新树形结构视图
+    def update_structure(self, xml_file):
+        self.xml_file = xml_file
+        title = 'None' if xml_file == 'None' else os.path.split(xml_file)[1]
+        self.root.setText(0, title)
+        current_tree_data = self.get_tree_data(xml_file)
+        current_tree_data = self.old_tree_data if len(current_tree_data) == 0 else current_tree_data
+        if len(self.old_tree_data) == 0:
+            self.new_structure(current_tree_data)
+            self.old_tree_data = current_tree_data
+            return
+        # 动态更新
+        self.dynamic_update_structure(current_tree_data)
+        # 保存数据
+        self.old_tree_data = current_tree_data
